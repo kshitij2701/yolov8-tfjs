ecbea6629224b3c08f7f652621a17f15f880ea2b
Dhruv <dhruv@Dhruvs-MacBook-Air.local>
2024-05-07 17:31:21 +0530

    first commit

---
+++ src/components/developerinfo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../style/DeveloperInfo.css";
+
+const DeveloperInfo = () => {
+  return (
+    <div className="developer-info">
+      <p>
+        Developed by <span className="developer-name">Kshitij Shishodia</span> and <span className="developer-name">Manish</span>
+      </p>
+    </div>
+  );
+};
+
+export default DeveloperInfo;
